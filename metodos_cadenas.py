a26b6b6b897fabd2cd14148c107f53b5d24e9e2c
marc0_7 <marcolefiman20@gmail.com>
2026-04-08 10:47:31 -0400

    Add files via upload

---
+++ metodos_cadenas.py
@@ -0,0 +1,12 @@
+nombre_personal = "Marco lefiman"
+tiulo_personal = " estudiante"
+ciudad = "Temuco"
+
+#El metodo DIR, nos indica todos los metodos disponibles para la variable 
+
+#print(dir(nombre_personal))
+
+print( f'su nombre CAPITALIZADO: {nombre_personal.capitalize()}')
+print( f' su nombre personal MAYUSCULAS: {nombre_personal.upper()}´)
+print( f´ su nombre personal como TITULO: {nombre_personal.title()}´)
+print( f´ ciudad en MINUSCULAS: {ciudad.lower()}´)
