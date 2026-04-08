062aa631f06c22fa2ec20d399eaff115f92d469c
marc0_7 <marcolefiman20@gmail.com>
2026-04-08 10:14:30 -0400

    Add files via upload

---
+++ inputs.py
@@ -0,0 +1,10 @@
+#cada dato ingresado meidante input es un TEXTO 
+numero = input ( "por favor , ingrese el numero entero...")
+print(type(numero))
+print(numero)
+434
+
+
+print(f" su NUMERO MULTIPLICADO por 2 es : {numero * 4 }")
+print(f" su NUMERO(convertido a entero) MULTIPLICADO por 2 es : {float( numero) * 2 }")
+print(f" su NUMERO(convertido a decimal) MULTIPLICADO por 2 es : {int( numero) * 2 }")
