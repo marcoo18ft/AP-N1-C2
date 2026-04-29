e57e00e7ebbf3bcce8f451086ab9a6439a338061
marcoo18ft <marcolefiman20@gmail.com>
2026-04-29 09:17:21 -0400

    listo

--- crear_funciones.py
+++ crear_funciones.py
@@ -1,6 +1,10 @@
 # Dentro del lenguaje, tenemos la opción de crear nuestras PROPIAS funciones
 # para eso usamos la palabra reservada DEF
 
+# DEF es acrínimo de DEFINIR, porque estamos definiendo una FUNCIÓN
+# Luego de DEF ponemos el NOMBRE de la función
+# Luego del NOMBRE de la FUNCIÓN ponemos sus ARGUMENTOS
+# Los ARGUMENTOS serán los insumos que usará la función para hacer su trabajo
 def suma(a,b):
     # Este será el contenido de la función
     resultado = a + b
@@ -25,29 +29,19 @@ def pedir_datos():
     str_numero_1 = input('Ingrese primer número: ')
     str_numero_2 = input('Inrese segundo número: ')
 
-    if str_numero_1.isdigit() and str_numero_2.isdigit():
-        num_1 = float(str_numero_1)
-        num_2 = float(str_numero_2)
+    num_1 = convertir_float(str_numero_1)
+    num_2 = convertir_float(str_numero_2)
+
+    if num_1 and num_2 != False:
+        return(num_1,num_2)
     else:
-        print('Valor NO corresponde a un número')
-    return(num_1,num_2)
+        print('Valor NO corresponde a un número!')
 
-
-# str_numero_1 = input('Ingrese primer número: ')
-# str_numero_2 = input('Inrese segundo número: ')
-
-# if str_numero_1.isdigit() and str_numero_2.isdigit():
-#     num_1 = float(str_numero_1)
-#     num_2 = float(str_numero_2)
-# else:
-#     print('Valor NO corresponde a un número')
-
-# a,b = pedir_datos()
-
-# suma(a,b)
-# resta(a,b)
-# multiplicacion(a,b)
-# division(a,b)
+def convertir_float(valor):
+    try:
+        return float(valor)
+    except (ValueError, TypeError):
+        return False
 
 
 print('\nBienvenido a Mi primera CALCULADORA')
@@ -62,20 +56,23 @@ while ciclo == True:
     print('[0] Salir')
     opcion = input('\nSeleccione su operación [0-4]: ')
 
-    if opcion == '0':
-        ciclo = False
-        print('Gracias por usar mi calculadora!')
-        print('Saliendo...')
-    else:
-        a,b = pedir_datos()
-        
-        if opcion == '1':
-            suma(a,b)
-        elif opcion == '2':
-            resta(a,b)
-        elif opcion == '3':
-            multiplicacion(a,b)
-        elif opcion == '4':
-            division(a,b)
+    opciones_validas = ['0','1','2','3','4']
+
+    if opcion in(opciones_validas):
+        if opcion == '0':
+            ciclo = False
+            print('Gracias por usar mi calculadora!')
+            print('Saliendo...')
         else:
-            print('Opción NO válida.')
+            a,b = pedir_datos()
+
+            if opcion == '1':
+                suma(a,b)
+            elif opcion == '2':
+                resta(a,b)
+            elif opcion == '3':
+                multiplicacion(a,b)
+            elif opcion == '4':
+                division(a,b)
+    else:
+        print('Opción NO válida.')
