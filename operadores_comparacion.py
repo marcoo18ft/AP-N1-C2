07534dc1e383d831ba19528965fddfdd45c4e115
marcoo18ft <marcolefiman20@gmail.com>
2026-05-05 10:55:26 -0400

    listo

---
+++ operadores_comparacion.py
@@ -0,0 +1,31 @@
+# Operador EQUIVALENTE
+print('\nOperador EQUIVALENTE')
+print(f'5 == 6 ? {5 == 6}')
+print(f'3 == 9/3 ? {3 == 9/3}')
+
+# Operador MAYOR QUE
+print('\nOperador MAYOR QUE')
+print(f'6 > 9 ? {6 > 9}')
+print(f'6 > 3 ? {6 > 3}')
+
+# Operador MAYOR O IGUAL QUE
+print('\nOperador MAYOR O IGUAL QUE')
+print(f'6 >= 3 ? {6 >= 3}')
+print(f'6 >= 9 ? {6 >= 9}')
+print(f'6 >= 12/2 ? {6 >= 12/2}')
+
+# Operador MENOR QUE
+print('\nOperador MENOR QUE')
+print(f'6 < 9 ? {6 < 9}')
+print(f'6 < 3 ? {6 < 3}')
+
+# Operador MENOR O IGUAL QUE
+print('\nOperador MENOR O IGUAL QUE')
+print(f'6 <= 3 ? {6 <= 3}')
+print(f'6 <= 9 ? {6 <= 9}')
+print(f'6 <= 12/2 ? {6 <= 12/2}')
+
+# Operador DISTINTO
+print('\nOperador DISTINTO')
+print(f'5 != 6 ? {5 != 6}')
+print(f'3 != 9/3 ? {3 != 9/3}')
